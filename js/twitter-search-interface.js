bc3cf6aae7045203bccf6d9e026d0d2add9b7ddf
Matthew <mreyez@gmail.com>
2016-08-04 21:05:36 -0700

    fix twitter search function

--- js/twitter-search-interface.js
+++ js/twitter-search-interface.js
@@ -3,22 +3,20 @@ var oAuthId = require("./../.env").oAuthId;
 OAuth.initialize(oAuthId);
 
 var twitterSearch = function() {
-  OAuth.popup('twitter').done(function(result) {
-    console.log(result);
-    result.get("https://api.twitter.com/1.1/statuses/user_timeline.json&count=2&oauth_consumer_key=Bxc0tpfwRWKh4irGuZLkTcQlA&oauth_nonce=53576f65edd6c6a1e8b00e0da47eab30&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1470353679&oauth_token=15154981-v7V6yBBTGASbebZToU84GFOObuMgqjIlbrt5RCupq&oauth_version=1.0&screen_name=realdonaldtrump")
-      .then(function(result){
-        console.log(result);
-      }).fail(function(error){
-        console.log(error);
-      });
-    // do some stuff with result
-
+  OAuth.popup('twitter', {cache: true})
+  .done(function(OAuthResult) {
+    OAuthResult.get("https://api.twitter.com/1.1/search/tweets.json?q=%40realDonaldTrump&count=10")
+  .done(function(response){
+    console.log(response);
+  })
+  .fail(function(error){
+    console.log(error);
+    });
   });
 };
 
 $(document).ready(function(){
   $("#search").click(function(){
     twitterSearch();
-
   });
 });
